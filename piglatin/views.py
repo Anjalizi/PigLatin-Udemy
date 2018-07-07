39da010e06b83dcde6afbb5c0834902eb56042f2
Anjali <aj2966@gmail.com>
2018-07-08 01:03:36 +0530

    page linkings

--- piglatin/views.py
+++ piglatin/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def hello(request):
-	return HttpResponse("Hello world!")
+def home(request):
+	return render(request, 'home.html')
+
+def translate(request):
+	return HttpResponse("You are on translate page")
